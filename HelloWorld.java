b2b4d746188b565b67a89001b2783c42c5802a2e
wenfeilin <143572411+wenfeilin@users.noreply.github.com>
2023-09-15 01:06:54 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     pen.println("Hello, GitHub!");
-    pen.flush();
+    pen.flush(); // new comment
   } // main(String[])
 } // class HelloWorld
